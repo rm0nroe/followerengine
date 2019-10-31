6fc5726d94943cc75cb3aa0bf0e78a3f5c80ea84
Ryan Monroe <rmonroe128@gmail.com>
2019-10-30 22:59:32 -0400

    initial commit

---
+++ main/main.py
@@ -0,0 +1,17 @@
+# https://machinelearningmastery.com/make-predictions-scikit-learn/
+# example of training a final classification model
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets.samples_generator import make_blobs
+# generate 2d classification dataset
+X, y = make_blobs(n_samples=100, centers=2, n_features=2, random_state=1)
+# fit final model
+model = LogisticRegression()
+model.fit(X, y)
+# new instances where we do not know the answer
+Xnew, _ = make_blobs(n_samples=3, centers=2, n_features=2, random_state=1)
+# make a prediction
+ynew = model.predict(Xnew)
+# show the inputs and predicted outputs
+for i in range(len(Xnew)):
+	print("X=%s, Predicted=%s" % (Xnew[i], ynew[i]))
+print("Launching!")
